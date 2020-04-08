31605775ec602cc5542afb3d5d170776941f079f
kjaer77 <losninos>
2020-04-08 14:05:34 +0200

    Opret bruger form lavet. Mangler funktionalitet

---
+++ WebApplication/Models/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class Customer
+    {
+
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Fornavn")]
+        public string firstName { get; set; }
+        [Required]
+        [DisplayName("Efternavn")]
+        public string lastName { get; set; }
+        [Required]
+        [DisplayName("Vej/gade")]
+        public string custStreet { get; set; }
+        [Required]
+        [DisplayName("Nummer")]
+        public int custNo { get; set; }
+        [Required]
+        [DisplayName("Postnummer")]
+        public string zipCode { get; set; }
+        [Required]
+        [DisplayName("Telefon")]
+        public string phoneNumber { get; set; }
+        [Required]
+        [DisplayName("Email")]
+        public string email { get; set; }
+        [Required]
+        [DisplayName("Password")]
+        public string password { get; set; }
+    }
+}
