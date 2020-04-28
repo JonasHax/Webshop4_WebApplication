c97296a30f8f6170129caa0d1636fd484a1bdd5a
Jonas Haxholm <jonashaxholm@hotmail.dk>
2020-04-28 13:13:55 +0200

    kommentarer til senere

--- WebApplication/Controllers/LoginController.cs
+++ WebApplication/Controllers/LoginController.cs
@@ -4,35 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace WebApplication.Controllers
-{
-    public class LoginController : Controller
-    {
+namespace WebApplication.Controllers {
+
+    public class LoginController : Controller {
+
         // GET: Login
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View();
         }
 
-        int customerID;
+        private int customerID;
 
         [HttpPost]
-        public ActionResult loginform(FormCollection collection)
-        {
+        public ActionResult loginform(FormCollection collection) {
+            // tjek på information
+
+            // find bruger
+
+            // tilføj bruger til en Session
+
             string email = collection.Get("email");
             string Password = collection.Get("Password");
-            if (email == "test@gmail.com" && Password == "123")
-            {
+            if (email == "test@gmail.com" && Password == "123") {
                 customerID = 5;
                 return View("LoginSuccess");
-            }
-            else
-            {
+            } else {
                 ViewBag.Message = string.Format("Please enter valid Email and Password!");
                 return View("Index");
             }
-           // return View("Index");
+            // return View("Index");
         }
     }
 }
-    
