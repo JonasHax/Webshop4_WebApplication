7135fffd485a4b33c97a1122c87bae761a15dfa3
kjaer77 <losninos>
2020-04-28 08:35:52 +0200

    ?

--- WebApplication/Models/Customer.cs
+++ WebApplication/Models/Customer.cs
@@ -4,43 +4,44 @@ using System.Linq;
 using System.Web;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using DO.Validation.Codes;
 
 namespace WebApplication.Models
 {
     public class Customer
     {
 
-        //public int Id { get; set; }
-        [Required(ErrorMessage = "Please enter first name.")]
+
+        [Required(ErrorMessage = "Indtast fornavn.")]
         [DisplayName("Fornavn")]
         public string FirstName { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indtast efternavn.")]
         [DisplayName("Efternavn")]
         public string LastName { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indtast vej/gade.")]
         [DisplayName("Vej/gade")]
         public string Street { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indtast nummer.")]
         [DisplayName("Nummer")]
         //public int CustNo { get; set; }
         public int HouseNo { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indtast postnummer.")]
         [DisplayName("Postnummer")]
         public string ZipCode { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indtast telefonnummer.")]
         [DisplayName("Telefon")]
         public string PhoneNumber { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indtast email.")]
         [DisplayName("Email")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Indtast password.")]
         [DisplayName("Password")]
         public string Password { get; set; }
     }
